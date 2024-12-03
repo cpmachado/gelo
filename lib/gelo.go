54e1fca04f689481aa3ff8f6cbff7d1f02e2b221
cpmachado <cpmachado@protonmail.com>
2024-12-03 17:09:41 +0000

    Refactor player out of lib

--- lib/gelo.go
+++ lib/gelo.go
@@ -10,37 +10,12 @@ import (
 	"net/http"
 	"os"
 	"path/filepath"
-	"strconv"
-	"strings"
+
+	"github.com/cpmachado/gelo/player"
 )
 
-type Player struct {
-	XMLName      xml.Name `xml:"player"`
-	Id           int      `xml:"fideid"`
-	Name         string   `xml:"name"`
-	Country      string   `xml:"country"`
-	Sex          string   `xml:"sex"`
-	Title        string   `xml:"title"`
-	W_title      string   `xml:"w_title"`
-	O_title      string   `xml:"o_title"`
-	Foa_title    string   `xml:"foa_title"`
-	Rating       int      `xml:"rating"`
-	Games        int      `xml:"games"`
-	K            int      `xml:"k"`
-	Rapid_rating int      `xml:"rapid_rating"`
-	Rapid_games  int      `xml:"rapid_games"`
-	Rapid_k      int      `xml:"rapid_k"`
-	Blitz_rating int      `xml:"blitz_rating"`
-	Blitz_games  int      `xml:"blitz_games"`
-	Blitz_k      int      `xml:"blitz_k"`
-	Birthday     string   `xml:"birthday"`
-	Flag         string   `xml:"flag"`
-}
-
-type Players struct {
-	XMLName xml.Name `xml:"playerslist"`
-	Players []Player `xml:"player"`
-}
+type Player = player.Player
+type Players = player.Players
 
 const (
 	dst        = "output"
@@ -49,34 +24,6 @@ const (
 	zipFile    = "output/players_list_xml.zip"
 	fideUrl    = "https://ratings.fide.com/download/players_list_xml.zip"
 )
-
-func (p Player) StringifiedRecords() []string {
-	return []string{
-		strconv.Itoa(p.Id),
-		p.Name,
-		p.Country,
-		p.Sex,
-		p.Title,
-		p.W_title,
-		p.O_title,
-		p.Foa_title,
-		strconv.Itoa(p.Rating),
-		strconv.Itoa(p.Games),
-		strconv.Itoa(p.K),
-		strconv.Itoa(p.Rapid_rating),
-		strconv.Itoa(p.Rapid_games),
-		strconv.Itoa(p.Rapid_k),
-		strconv.Itoa(p.Blitz_rating),
-		strconv.Itoa(p.Blitz_games),
-		strconv.Itoa(p.Blitz_k),
-		p.Birthday,
-		p.Flag,
-	}
-}
-
-func (p Player) String() string {
-	return strings.Join(p.StringifiedRecords(), ";")
-}
 
 func writeCsv(players Players) {
 	n := len(players.Players)
---
+++ player/player.go
@@ -0,0 +1,64 @@
+/* Copyright © 2024 Carlos Pinto Machado<cpmachado@protonmail.com> */
+package player
+
+import (
+	"encoding/xml"
+	"strconv"
+	"strings"
+)
+
+type Players struct {
+	XMLName xml.Name `xml:"playerslist"`
+	Players []Player `xml:"player"`
+}
+
+type Player struct {
+	XMLName      xml.Name `xml:"player"`
+	Id           int      `xml:"fideid"`
+	Name         string   `xml:"name"`
+	Country      string   `xml:"country"`
+	Sex          string   `xml:"sex"`
+	Title        string   `xml:"title"`
+	W_title      string   `xml:"w_title"`
+	O_title      string   `xml:"o_title"`
+	Foa_title    string   `xml:"foa_title"`
+	Rating       int      `xml:"rating"`
+	Games        int      `xml:"games"`
+	K            int      `xml:"k"`
+	Rapid_rating int      `xml:"rapid_rating"`
+	Rapid_games  int      `xml:"rapid_games"`
+	Rapid_k      int      `xml:"rapid_k"`
+	Blitz_rating int      `xml:"blitz_rating"`
+	Blitz_games  int      `xml:"blitz_games"`
+	Blitz_k      int      `xml:"blitz_k"`
+	Birthday     string   `xml:"birthday"`
+	Flag         string   `xml:"flag"`
+}
+
+func (p Player) StringifiedRecords() []string {
+	return []string{
+		strconv.Itoa(p.Id),
+		p.Name,
+		p.Country,
+		p.Sex,
+		p.Title,
+		p.W_title,
+		p.O_title,
+		p.Foa_title,
+		strconv.Itoa(p.Rating),
+		strconv.Itoa(p.Games),
+		strconv.Itoa(p.K),
+		strconv.Itoa(p.Rapid_rating),
+		strconv.Itoa(p.Rapid_games),
+		strconv.Itoa(p.Rapid_k),
+		strconv.Itoa(p.Blitz_rating),
+		strconv.Itoa(p.Blitz_games),
+		strconv.Itoa(p.Blitz_k),
+		p.Birthday,
+		p.Flag,
+	}
+}
+
+func (p Player) String() string {
+	return strings.Join(p.StringifiedRecords(), ";")
+}

27380a6f5b105eabb70e69bb0befca4721066483
cpmachado <cpmachado@protonmail.com>
2025-10-01 23:44:57 +0100

    completely bomb previous implementation

--- fide/urls.go
+++
@@ -1,8 +0,0 @@
-package fide
-
-const (
-	// XmlURL refers to the current FIDE rating List in XML
-	XmlURL = "https://ratings.fide.com/download/players_list_xml.zip"
-	// TxtURL refers to the current FIDE rating List
-	TxtURL = "https://ratings.fide.com/download/players_list.zip"
-)

283bd06d0944aaacda8a62a8b53e3664408d2875
cpmachado <cpmachado@protonmail.com>
2025-10-02 00:58:40 +0100

    add config options

--- cmd/root.go
+++ cmd/root.go
@@ -6,9 +6,14 @@ import (
 	"runtime/debug"
 
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+	"go.cpmachado.pt/gelo/internal/config"
 )
 
-var runVersionFlag bool
+var (
+	runVersionFlag bool
+	cfgFile        string = config.DefaultConfigFile
+)
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
@@ -31,10 +36,32 @@ var rootCmd = &cobra.Command{
 func Execute() {
 	err := rootCmd.Execute()
 	if err != nil {
+		fmt.Fprint(os.Stderr, err.Error())
 		os.Exit(1)
 	}
 }
 
 func init() {
+	cobra.OnInitialize(initConfig)
 	rootCmd.Flags().BoolVarP(&runVersionFlag, "version", "v", false, "Display version")
+	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", config.DefaultConfigFile, "config file")
+}
+
+func initConfig() {
+	if cfgFile != "" {
+		// Use config file from the flag.
+		viper.SetConfigFile(cfgFile)
+	} else {
+
+		// Search config in home directory with name ".cobra" (without extension).
+		viper.AddConfigPath(config.DefaultConfigHome)
+		viper.SetConfigType("json")
+		viper.SetConfigName("config")
+	}
+
+	viper.AutomaticEnv()
+
+	if err := viper.ReadInConfig(); err == nil {
+		fmt.Println("Using config file:", viper.ConfigFileUsed())
+	}
 }

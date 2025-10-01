c416ce3000e347ecf9eee57ed2b24359cf4a62c0
cpmachado <cpmachado@protonmail.com>
2025-10-01 23:41:54 +0100

    Add version command

--- cmd/root.go
+++ cmd/root.go
@@ -1,15 +1,31 @@
 package cmd
 
 import (
+	"fmt"
 	"os"
+	"runtime/debug"
 
 	"github.com/spf13/cobra"
 )
+
+var runVersionFlag bool
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "gelo",
 	Short: "A tool to keep up with ratings",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		if !runVersionFlag {
+			return cmd.Help()
+		}
+		progBuildInfo, ok := debug.ReadBuildInfo()
+		if ok {
+			fmt.Printf("%s-%s", cmd.Use, progBuildInfo.Main.Version)
+		} else {
+			fmt.Printf("%s-unknown", cmd.Use)
+		}
+		return nil
+	},
 }
 
 func Execute() {
@@ -17,4 +33,8 @@ func Execute() {
 	if err != nil {
 		os.Exit(1)
 	}
+}
+
+func init() {
+	rootCmd.Flags().BoolVarP(&runVersionFlag, "version", "v", false, "Display version")
 }
